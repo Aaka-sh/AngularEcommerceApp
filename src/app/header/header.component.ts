3d6895518e22377bb3e005cce69bc0740e924027
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-01-29 13:25:11 +0530

    Added Logout functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,22 +8,32 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  //seller name
+  sellerName: string = '';
   //creating a flag-type variable to track whether the seller is logged in or not
   menuType: string = 'default';
   //creating an instance of Router service
   constructor(private route: Router) {}
-  ngOnInit() {
+  ngOnInit(): void {
     //events method is an observable that emits events related to navigation changes
     this.route.events.subscribe((val: any) => {
       if (val.url) {
         if (localStorage.getItem('seller') && val.url.includes('seller')) {
-          console.warn('Inside');
+          let sellerStore = localStorage.getItem('seller');
+          //this will check if the sellerstore is empty before assigning value to sellerdata
+          let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+          this.sellerName = sellerData.name;
           this.menuType = 'seller';
         } else {
-          console.warn('Outside');
           this.menuType = 'default';
         }
       }
     });
+  }
+
+  //logout function
+  logout() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
   }
 }
