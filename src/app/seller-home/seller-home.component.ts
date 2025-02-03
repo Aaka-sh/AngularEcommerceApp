4ab52f53def43cbf8bb71ccb605e2387b05c0edf
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-02-03 16:51:48 +0530

    Display the product list

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -10,11 +10,34 @@ import { product } from '../data-type';
   styleUrl: './seller-home.component.css',
 })
 export class SellerHomeComponent implements OnInit {
+  //productList will hold the api result
   productList: undefined | product[];
+  //productMessage after the product is deleted
+  productMessage: undefined | string;
   //instance of product service
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
+    //call api when the page is first loaded
+    this.list();
+  }
+
+  deleteProduct(id: string): void {
+    console.log(id);
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product deleted successfully';
+        //call the api after the product has been deleted
+        this.list();
+      }
+    });
+    //setTimeout function displays the error message for 3 seconds
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+  }
+
+  list() {
     //call the api to get the product list
     this.product.productList().subscribe((result) => {
       console.warn(result);
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -18,4 +18,8 @@ export class ProductService {
     //typecast the response into an array of product (interface)
     return this.http.get<product[]>('http://localhost:3000/products');
   }
+
+  deleteProduct(id: string) {
+    return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
 }
