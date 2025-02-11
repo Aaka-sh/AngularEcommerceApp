d8967ae8dfa7ef822df5744d26a19dbf91fbb3c6
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-02-11 15:29:20 +0530

    Product Details UI adde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './search/search.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { SearchComponent } from './search/search.component';
     SellerAddProductComponent,
     SellerUpdateProductComponent,
     SearchComponent,
+    ProductDetailsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule, NgbModule],
   providers: [
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-search',
   standalone: false,
-  
-  templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
-})
-export class SearchComponent {
 
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent implements OnInit {
+  searchResult: undefined | product[];
+  //activated route service is used to access information about the current route (URL)
+  //route parameter and query parameters
+  //we need to extract the search keyword from the route (URL)
+
+  //product service instance is created to call the searchProduct method
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private product: ProductService
+  ) {}
+  ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    //console.warn('from the query parameter: ',query);
+    query &&
+      this.product.searchProduct(query).subscribe((result) => {
+        this.searchResult = result;
+      });
+  }
 }
