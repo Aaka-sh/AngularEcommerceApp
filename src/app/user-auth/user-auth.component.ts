67f8d1317c12d738e742c44036083653ec63290a
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-02-27 16:49:56 +0530

    Transferred contents from local storage to db.json

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { login, signUp } from '../data-type';
+import { cart, login, product, signUp } from '../data-type';
 import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -14,30 +15,66 @@ export class UserAuthComponent implements OnInit {
   authError: string = '';
   //flag to show the login form or sign up form
   showLogin: boolean = true;
-  constructor(private user: UserService) {}
+
+  constructor(private user: UserService, private product: ProductService) {}
   ngOnInit(): void {
     this.user.userAuthReload();
   }
 
+  //user sign up
   signUp(data: signUp) {
     this.user.userSignUp(data);
   }
 
+  //user login
   login(data: login) {
     this.user.userLogin(data);
     this.user.isLoginEmitter.subscribe((isError) => {
       console.warn(isError);
       if (isError) {
         this.authError = 'Incorrect Credentials';
+      } else {
+        this.localCartToRemoteCart();
       }
     });
   }
 
+  //hides form when needed
   openSignup() {
     this.showLogin = false;
   }
 
+  //shows login form when needed
   openLogin() {
     this.showLogin = true;
+  }
+
+  //transfer contents of cart from local storage to database
+  localCartToRemoteCart() {
+    //getting the local cart contents
+    let data = localStorage.getItem('localCart');
+    if (data) {
+      let cartListData: product[] = JSON.parse(data);
+      let user = localStorage.getItem('user');
+      let userId = user && JSON.parse(user).id;
+      cartListData.forEach((product: product, index) => {
+        let cartData: cart = {
+          ...product,
+          productId: product.id,
+          userId,
+        };
+        delete cartData.id;
+        setTimeout(() => {
+          this.product.addToCart(cartData).subscribe((result) => {
+            if (result) {
+              console.log('Data transferred from local storage to DB');
+            }
+          });
+        }, 500);
+        if (cartListData.length === index + 1) {
+          localStorage.removeItem('localCart');
+        }
+      });
+    }
   }
 }
